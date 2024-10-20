33c70cf0b677c19a797e94db0ce4c09cf6a0db05
oyerohabib <oyerohabib@yahoo.com>
2024-10-20 14:40:02 +0100

    fix: completed products section desktop

--- src/app/components/Homepage/Products.tsx
+++ src/app/components/Homepage/Products.tsx
@@ -1,22 +1,54 @@
+import Image from "next/image";
+
 export const Products = () => {
   const products = [
-    { name: "Investment 1", price: "$500", status: "Available" },
-    { name: "Investment 2", price: "$600", status: "Available" },
-    { name: "Investment 3", price: "$700", status: "Available" },
+    {
+      name: "Pharetra diam vitae duis vash aliquet",
+      units: "500",
+      status: "Available",
+      image: "/images/products.svg",
+    },
+    {
+      name: "Pharetra diam vitae duis vash aliquet",
+      units: "600",
+      status: "Available",
+      image: "/images/products.svg",
+    },
+    {
+      name: "Pharetra diam vitae duis vash aliquet",
+      units: "700",
+      status: "Available",
+      image: "/images/products.svg",
+    },
+    {
+      name: "Pharetra diam vitae duis vash aliquet",
+      units: "700",
+      status: "Available",
+      image: "/images/products.svg",
+    },
   ];
 
   return (
-    <section className="py-16 px-4 bg-green-100">
+    <section className=" py-16 px-24 w-full bg-secondary-green">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-semibold text-center mb-8">
+        <h2 className="text-3xl font-extrabold text-center mb-8">
           Our Latest Investment Products
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
           {products.map((product, index) => (
-            <div key={index} className="bg-white p-4 rounded shadow-md">
-              <h3 className="text-lg font-semibold">{product.name}</h3>
-              <p className="text-gray-600">{product.price}</p>
-              <p className="text-green-600">{product.status}</p>
+            <div key={index} className="p-4 space-y-2">
+              <Image
+                src={product.image}
+                alt="product"
+                width={300}
+                height={200}
+                className="w-full"
+              />
+              <h3 className="font-medium max-w-52">{product.name}</h3>
+              <p className="text-gray-600">
+                {product.units} Units.{" "}
+                <span className="text-green-600">{product.status}</span>
+              </p>
             </div>
           ))}
         </div>
