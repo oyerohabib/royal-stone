e33f1014e18b5c4f28827d2bfa48104837af4f0d
oyerohabib <oyerohabib@yahoo.com>
2024-10-20 10:31:26 +0100

    fix: completed services section desktop

--- src/app/components/Homepage/Services.tsx
+++ src/app/components/Homepage/Services.tsx
@@ -1,27 +1,81 @@
+import Image from "next/image";
+
 export const Services = () => {
   return (
-    <section className="py-16 px-4" id="services">
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-semibold mb-8">Our Services</h2>
-        <div className="grid md:grid-cols-2 gap-8">
-          <div className="bg-white p-6 rounded shadow-md">
-            <h3 className="text-xl font-semibold">Make an Investment</h3>
-            <p className="mt-2 text-gray-600">
-              Amet commodo venenatis in congue. Sit eget nulla neque.
-            </p>
+    <section className="flex flex-col md:flex-row justify-between gap-6 pt-16 px-24 w-full">
+      <div className="w-1/2 flex flex-col border-r">
+        <div className="max-w-lg">
+          <h2 className="text-3xl font-extrabold mb-4">Our Services</h2>
+          <p className="mt-2 text-light-black mb-10">
+            Proin quam varius facilisis urna. Viverra at vitae lacus at ut
+            volutpat. Amet commodo venenatis in congue. Sit eget nullam nunc
+            neque nulla venenatis. Felis ornare nulla eros dolor viverra quis
+            odio or
+          </p>
+
+          <Image
+            src={"/images/services.svg"}
+            alt="dashboard services"
+            width={100}
+            height={100}
+            className="w-full"
+          />
+        </div>
+      </div>
+      <div className="w-1/2 flex flex-col gap-5 px-5">
+        <div className="flex flex-col gap-4 max-w-2xl bg-lighter-gray p-6 rounded-3xl">
+          <div className="border p-2 rounded-xl -rotate-45 w-fit">
+            <Image
+              src={"/images/chart.svg"}
+              alt="target"
+              width={50}
+              height={50}
+              className="size-6 rotate-45"
+            />
           </div>
-          <div className="bg-white p-6 rounded shadow-md">
-            <h3 className="text-xl font-semibold">Create a Savings Target</h3>
-            <p className="mt-2 text-gray-600">
-              Sit eget nulla nulla neque nulla venenatis facilisis.
-            </p>
+          <h3 className="text-lg font-semibold">Make an Investment</h3>
+          <p className="text-light-black pr-4">
+            Proin quam varius facilisis urna. Viverra at vitae lacus at ut
+            volutpat. Amet commodo venenatis in congue. Sit eget nullam nunc
+            neque nulla venenatis. Felis ornare nulla eros dolor viverra quis
+            odio or
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 max-w-2xl bg-lighter-gray p-6 rounded-3xl">
+          <div className="border p-2 rounded-xl -rotate-45 w-fit">
+            <Image
+              src={"/images/target.svg"}
+              alt="target"
+              width={50}
+              height={50}
+              className="size-6 rotate-45"
+            />
           </div>
-          <div className="bg-white p-6 rounded shadow-md">
-            <h3 className="text-xl font-semibold">Invest in Stocks</h3>
-            <p className="mt-2 text-gray-600">
-              Eget nulla nulla neque nulla venenatis facilisis.
-            </p>
+          <h3 className="text-lg font-semibold">Create a Savings Target</h3>
+          <p className="text-light-black pr-4">
+            Proin quam varius facilisis urna. Viverra at vitae lacus at ut
+            volutpat. Amet commodo venenatis in congue. Sit eget nullam nunc
+            neque nulla venenatis. Felis ornare nulla eros dolor viverra quis
+            odio or
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 max-w-2xl bg-lighter-gray p-6 rounded-3xl">
+          <div className="border p-2 rounded-xl -rotate-45 w-fit">
+            <Image
+              src={"/images/stocks.svg"}
+              alt="target"
+              width={50}
+              height={50}
+              className="size-6 rotate-45"
+            />
           </div>
+          <h3 className="text-lg font-semibold">Invest in Stocks</h3>
+          <p className="text-light-black pr-4">
+            Proin quam varius facilisis urna. Viverra at vitae lacus at ut
+            volutpat. Amet commodo venenatis in congue. Sit eget nullam nunc
+            neque nulla venenatis. Felis ornare nulla eros dolor viverra quis
+            odio or
+          </p>
         </div>
       </div>
     </section>
