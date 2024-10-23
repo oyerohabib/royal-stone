1b624392a3888eeb123a512575c0a937459051bc
oyerohabib <oyerohabib@yahoo.com>
2024-10-23 10:42:55 +0100

    fix: improved navbar by making it SSR

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,19 +1,7 @@
-"use client";
 import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-const pageLinks = [
-  { id: 1, title: "Home", path: "/" },
-  { id: 2, title: "About Us", path: "/about-us" },
-  { id: 3, title: "Contact Us", path: "/contact-us" },
-  { id: 4, title: "Privacy Policy", path: "/privacy-policy" },
-  { id: 5, title: "Terms Of Service", path: "/terms-of-service" },
-];
+import { NavLinks } from "./NavLinks";
 
 export const Navbar = () => {
-  const pathname = usePathname();
-
   return (
     <nav className="flex justify-between items-center shadow-md px-4 md:px-12 xl:px-24 py-6 bg-secondary-green">
       <div className="flex items-center">
@@ -34,28 +22,7 @@ export const Navbar = () => {
           className="size-7"
         />
       </div>
-      <ul className="hidden md:flex space-x-4 xl:space-x-6 items-center text-light-black">
-        {pageLinks.map((pageObj) => {
-          return (
-            <li key={pageObj.id}>
-              <Link
-                href={pageObj.path}
-                className={`${
-                  pathname === pageObj.path ? "text-primary-blue font-bold" : ""
-                } 
-              ${
-                pageObj.path === "/terms-of-service" ||
-                pageObj.path === "/privacy-policy"
-                  ? "hidden lg:block"
-                  : ""
-              }`}
-              >
-                {pageObj.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <NavLinks />
       <div className="hidden md:flex items-center space-x-[10px]">
         <button className="text-primary-green px-5 py-2 rounded-lg bg-white shadow-sm font-semibold">
           Log In
